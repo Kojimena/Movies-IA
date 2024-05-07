dc6b402a3d5173d8d4e0ecc717252b7ed4977e00
Jimena Hernández <her21199@uvg.edu.gt>
2024-05-06 19:46:07 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+ /** @type {import('tailwindcss').Config} */
+ module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    screens: {
+      'xs': '320px',
+      
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['inter', 'sans-serif'],
+        montserrat: ['montserrat', 'sans-serif'],
+        anton : ['anton', 'sans-serif'],
+      },
+      colors: {
+        "darkgray": "#262626",
+        "lightgray": "#F2F2F2",
+        "lightergray": "#EBEBEB",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+}
